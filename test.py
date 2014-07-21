f76222d89f67d204c8110043211912a8aeb4c7e0
aigeano <aigeano@gmail.com>
2014-07-22 02:30:48 +0530

    Parsing Complete. Phew git add .

--- test.py
+++ test.py
@@ -2,6 +2,8 @@ import re, requests, sys
 from bs4 import BeautifulSoup, NavigableString
 import bleach
 def strip_tags(data,inv):
+
+	pattern = ['&lt','&gt','/[.*/]','html','body',';',':','/']
 	soup = BeautifulSoup(data)
 	for tag in soup.findAll(True):
 		if tag.name in inv:
@@ -11,20 +13,25 @@ def strip_tags(data,inv):
 					c = strip_tags(unicode(c),inv)
 				s +=unicode(c)
 			tag.replaceWith(s)
-	return soup
 
+	p = soup.findAll('p')
+	for para in p:
+
+		p1 = bleach.clean(para, strip = True)
+
+	        p2 = re.sub(pattern[0],"",p1)
+
+	        for pat in pattern :
+
+			p2 = re.sub(pat,"",p2)
+
+	        print p2
 
 def main():
 	r = requests.get("http://en.wikipedia.org/wiki/google")
 	data= r.text
-	inv = ['p','a','span','h2','img','sup']
-	pattern = ['&lt','&gt','/[.*/]','html','body',';',':','/']
+	inv = ['a','span','h2','img','sup','i','b']
         soup = BeautifulSoup(data)
-	p = strip_tags(data,inv)
-	p1 = bleach.clean(p, strip = True)
-	p2 = re.sub(pattern[0],"",p1)
-	for pat in pattern :
-		p2 = re.sub(pat,"",p2)
-	print p2
+	strip_tags(data,inv)
 if __name__=='__main__':
         main()
